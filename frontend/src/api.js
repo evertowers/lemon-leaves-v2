03590ab8f1b196ebec934594318cb84bef46b5e7
Everly Torres <72683137+evertowers@users.noreply.github.com>
2025-01-05 14:40:56 +0800

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8000/api/auth';
-
-export const register = (userData) => axios.post(`${API_URL}/register`, userData);
-export const login = (userData) => axios.post(`${API_URL}/login`, userData);
-export const upload = (imageData) => axios.post(`${API_URL}/predict`, imageData);
