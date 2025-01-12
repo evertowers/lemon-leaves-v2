c7f99bdb3bcf4934b5574335ff557d3613d65be3
evertowers <everlycabilizatorres@gmail.com>
2025-01-12 14:26:53 +0800

    2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
-
 const API_URL = process.env.REACT_APP_API_URL || "https://legal-tammi-student0617-c9760f79.koyeb.app";
 
 export const register = (userData) => axios.post("https://legal-tammi-student0617-c9760f79.koyeb.app/api/auth/register", userData);
